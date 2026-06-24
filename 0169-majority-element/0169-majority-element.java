94f6bdee9c37f78d618e7b104620c01682988488
Anmol Gupta <167634091+Anm0lGupta@users.noreply.github.com>
2026-06-24 17:40:56 +0530

    Time: 1 ms (99.89%), Space: 55.6 MB (59.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+        //BoyerMooreVolting Algo
+        int count = 0;
+        int candidate = 0;
+        for(int num: nums)
+        {
+            if(count==0)
+            {
+                candidate = num;
+            }
+            count += (num==candidate) ? 1:-1;
+        }
+        return candidate;
     }
 }
