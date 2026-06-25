b576fbf8b6c3fb623ae989dfa0a669361f406461
Anmol Gupta <167634091+Anm0lGupta@users.noreply.github.com>
2026-06-26 00:02:43 +0530

    Time: 2 ms (88.84%), Space: 47.9 MB (61.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,33 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        if(nums.length == 1)
+        int maxReach = 0;
+        for(int i = 0; i < nums.length; i++)
         {
-            return true;
-        }
-        if(nums[0] == 0)
-        {
-            return false;
-        }
-        for(int i=0; i< nums.length-1; i++)
-        {
-            if(nums[i] == 0)
+            if(i > maxReach)
             {
-                boolean canSurpass = false;
-                for(int k=i-1; k>=0; k--)
-                {
-                    if(k+nums[k] >i)
-                    {
-                        canSurpass = true;
-                        break;
-                    }
-                }
-                if(!canSurpass)
-                {
-                    return false;
-                }
+                return false;
             }
-            
+            if(i+nums[i] >= nums.length-1)
+            {
+                return true;
+            }
+            maxReach = Math.max(i+nums[i], maxReach);
+         
         }
-        return true;
+       return false;
     }
 }
