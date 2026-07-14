7e61889e01e80abdf463f32f9b1167adad6ac5d1
Anmol Gupta <167634091+Anm0lGupta@users.noreply.github.com>
2026-07-14 17:54:58 +0530

    Time: 30 ms (66.2%), Space: 77.1 MB (19.8%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,20 +1,26 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int[] sorted = arr.clone();
-        Arrays.sort(sorted);
-
-        int m = 0;
-        for (int x : sorted) {
-            if (m == 0 || sorted[m - 1] != x) {
-                sorted[m++] = x;
+        int[] A = arr.clone();
+        Arrays.sort(A);
+        Map<Integer, Integer> mp = new HashMap<>();
+        int count = 1;
+        for(int num : A)
+        {
+            boolean check = mp.containsKey(num);
+            if(!check)
+            {
+                mp.put(num, count);
+                count++;
+            }
+            else
+            {
+                continue;
             }
         }
-
-        int[] unique = Arrays.copyOf(sorted, m);
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Arrays.binarySearch(unique, arr[i]) + 1;
+        for(int i=0; i<A.length; i++)
+        {
+            A[i] = mp.get(arr[i]);
         }
-
-        return arr;
+        return A;
     }
 }
