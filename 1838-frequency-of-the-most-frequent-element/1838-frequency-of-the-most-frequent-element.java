4a591fc6ab50261f1c89be49704ab2ed47ad5534
Anmol Gupta <167634091+Anm0lGupta@users.noreply.github.com>
2026-08-08 11:47:38 +0530

    Time: 33 ms (91.96%), Space: 95 MB (13.62%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,42 +1,21 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        int n = nums.length; 
-        Arrays.sort(nums); 
-        int result = 0; 
-        long[] prefixSum = new long[n];
-        prefixSum[0] = nums[0];
-        for(int i=1; i<n; i++)
+        int n = nums.length;
+        Arrays.sort(nums);
+        int l = 0;
+        int result = 0;
+        long currSum = 0;
+        for(int r=0; r<n; r++)
         {
-            prefixSum[i] = nums[i] + prefixSum[i-1];
-        }
-        for(int target_idx = 0; target_idx < n; target_idx++)
-        {
-            result = Math.max(result, binary(target_idx, k, prefixSum, nums));
+            long target = nums[r];
+            currSum += nums[r];
+            while((r-l+1)*target - currSum > k)
+            {
+                currSum -= nums[l];
+                l++;
+            }
+            result = Math.max(result, r-l+1);
         }
         return result;
-    }
-    static int binary(int target_idx, int k, long[] prefixSum, int[] nums)
-    {
-        int l = 0;
-        int r = target_idx;
-        int best_idx = target_idx;
-        while(l<=r)
-        {
-            int mid = l + (r-l)/2;
-            long count = target_idx - mid + 1;
-            long windowSum = count*nums[target_idx];
-            long currSum = prefixSum[target_idx] - prefixSum[mid] + nums[mid];
-            long ops = windowSum - currSum;
-            if(ops>k)
-            {
-                l = mid+1;
-            }
-            else
-            {
-                best_idx = mid;
-                r = mid-1;
-            }
-        }
-        return target_idx - best_idx + 1;
     }
 }
